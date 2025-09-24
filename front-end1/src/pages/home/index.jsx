6135f056a4e5ec4839e60b4d8c3e16031d680e40
Dani <daniellytoledosouto@gmail.com>
2025-09-24 21:01:37 +0100

    CSS do Front-End feita

--- front-end1/src/pages/home/index.jsx
+++ front-end1/src/pages/home/index.jsx
@@ -1,28 +1,45 @@
 import './styles.css'
-import Lixeira from '../../assets/lixeira.png'
+import Lixeira1 from '../../assets/lixeira1.png'
 
 function Home() {
+  const users = [
+    {
+      id: '97329371',
+      name: 'Julia',
+      age: 33,
+      email: 'julia@email.com'
+    },
+    {
+      id: '98342984',
+      name: 'Malaica',
+      age: 33,
+      email: 'mama@email.com',
+    },
+  ]
 
   return (
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text' />
-        <input idade='email' type='number' />
-        <input name='nome' type='email'/>
+        <input placeholder='Nome' name='nome' type='text' />
+        <input placeholder='Idade' idade='email' type='number' />
+        <input placeholder='E-mail' name='nome' type='email' />
         <button type='button'>Cadastrar</button>
       </form>
 
-      <div>
+      {users.map(user => (
+        <div key={user.id} className='card'>
           <div>
-            <p>Nome:</p>
-            <p>Idade:</p>
-            <p>E-mail:</p>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>E-mail: <span>{user.email}</span></p>
           </div>
           <button>
-            <img src={Lixeira} />
+            <img src={Lixeira1} />
           </button>
-      </div>
+        </div>
+      ))}
+
     </div>
   )
 }
