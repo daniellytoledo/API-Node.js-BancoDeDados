77c8bdf1d205cf401046ff80e41b1f5a4d33350f
Dani <daniellytoledosouto@gmail.com>
2025-09-28 17:11:00 +0100

    corrigindo a conexão

--- front-end1/src/pages/home/index.jsx
+++ front-end1/src/pages/home/index.jsx
@@ -3,49 +3,55 @@ import './styles.css'
 import Lixeira1 from '../../assets/lixeira1.png'
 import api from '../../services/api'
 
-
 function Home() {
-    const [users, setUsers] = useState([])  // armazenar usuários
+    const [users, setUsers] = useState([])
 
     async function getUsers() {
         try {
             const response = await api.get('/usuarios')
-            setUsers(response.data)  // Armazena os dados no estado
+            setUsers(response.data)
             console.log('Usuários carregados:', response.data)
         } catch (error) {
             console.error('Erro ao buscar usuários:', error)
         }
-  }
+    }
 
-  useEffect(() => {
-    getUsers()
-  }, [])
+    useEffect(() => {
+        getUsers()
+    }, [])
 
-  return (
-    <div className='container'>
-      <form>
-        <h1>Cadastro de Usuários</h1>
-        <input placeholder='Nome' name='nome' type='text' />
-        <input placeholder='Idade' idade='email' type='number' />
-        <input placeholder='E-mail' name='nome' type='email' />
-        <button type='button'>Cadastrar</button>
-      </form>
+    return (
+        <div className='container'>
+            <form>
+                <h1>Cadastro de Usuários</h1>
+                <input placeholder='Nome' name='name' type='text' />
+                <input placeholder='Idade' name='age' type='number' />
+                <input placeholder='E-mail' name='email' type='email' />
+                <button type='button'>Cadastrar</button>
+            </form>
 
-      {users.map(user => (
-        <div key={user.id} className='card'>
-          <div>
-            <p>Nome: <span>{user.name}</span></p>
-            <p>Idade: <span>{user.age}</span></p>
-            <p>E-mail: <span>{user.email}</span></p>
-          </div>
-          <button>
-            <img src={Lixeira1} />
-          </button>
+            {/* VERIFICAÇÃO DOS DADOS */}
+            {console.log('Users no estado:', users)}
+            
+            {users.length > 0 ? (
+                users.map(user => (
+                    <div key={user.id} className='card'>
+                        <div>
+                            {/* Tente ambas as formas - inglês e português */}
+                            <p>Nome: <span>{user.name || user.nome}</span></p>
+                            <p>Idade: <span>{user.age || user.idade}</span></p>
+                            <p>E-mail: <span>{user.email || user.email}</span></p>
+                        </div>
+                        <button>
+                            <img src={Lixeira1} alt="Excluir" />
+                        </button>
+                    </div>
+                ))
+            ) : (
+                <p>Nenhum usuário cadastrado</p>
+            )}
         </div>
-      ))}
-
-    </div>
-  )
+    )
 }
 
-export default Home;
+export default Home
