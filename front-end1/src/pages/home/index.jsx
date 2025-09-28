7f85cf1853e168d2f606851849bfd095a6840ece
Dani <daniellytoledosouto@gmail.com>
2025-09-28 17:55:17 +0100

    delete button

--- front-end1/src/pages/home/index.jsx
+++ front-end1/src/pages/home/index.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './styles.css'
 import Lixeira1 from '../../assets/lixeira1.png'
 import api from '../../services/api'
 
 function Home() {
     const [users, setUsers] = useState([])
+
+    const inputName = useRef()
+    const inputAge = useRef()
+    const inputEmail = useRef()
 
     async function getUsers() {
         try {
@@ -16,6 +20,32 @@ function Home() {
         }
     }
 
+    async function createUsers() {
+        await api.post('usuarios', {
+            name: inputName.current.value,
+            age: inputAge.current.value,
+            email: inputEmail.current.value
+        })
+
+        getUsers()
+    }
+
+    async function deleteUsers(id) {
+        try {
+            console.log('🗑️ Deletando usuário ID:', id)
+            await api.delete(`/usuarios/${id}`)
+            console.log('✅ Usuário deletado, atualizando lista...')
+            getUsers()
+        } catch (error) {
+            console.error('❌ Erro ao deletar usuário:', error)
+        }
+    }
+
+    useEffect(() => {
+        console.log('🎯 Componente montado, buscando usuários...')
+        getUsers()
+    }, [])
+
     useEffect(() => {
         getUsers()
     }, [])
@@ -24,10 +54,10 @@ function Home() {
         <div className='container'>
             <form>
                 <h1>Cadastro de Usuários</h1>
-                <input placeholder='Nome' name='name' type='text' />
-                <input placeholder='Idade' name='age' type='number' />
-                <input placeholder='E-mail' name='email' type='email' />
-                <button type='button'>Cadastrar</button>
+                <input placeholder='Nome' name='name' type='text' ref={inputName}/>
+                <input placeholder='Idade' name='age' type='number' ref={inputAge}/>
+                <input placeholder='E-mail' name='email' type='email' ref={inputEmail}/>
+                <button type='button' onClick={createUsers}>Cadastrar</button>
             </form>
 
             {/* VERIFICAÇÃO DOS DADOS */}
@@ -42,8 +72,8 @@ function Home() {
                             <p>Idade: <span>{user.age || user.idade}</span></p>
                             <p>E-mail: <span>{user.email || user.email}</span></p>
                         </div>
-                        <button>
-                            <img src={Lixeira1} alt="Excluir" />
+                        <button onClick={() => deleteUsers(user.id)}>
+                            <img src={Lixeira1} />
                         </button>
                     </div>
                 ))
