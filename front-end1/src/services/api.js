dd269e175572fe52a5367b555afa372886500165
Dani <daniellytoledosouto@gmail.com>
2025-11-20 15:04:58 +0000

    baseURL
    https://api-node-js-express-react.onrender.com/

--- front-end1/src/services/api.js
+++ front-end1/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://api-node-js-express-react.onrender.com/'
 })
 
 export default api
